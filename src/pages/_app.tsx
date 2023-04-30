cb081ae8c3e6ebb827a985425ffe65e6285b31bf
chibataku0815 <chiba@fores-tone.co.jp>
2023-04-30 15:38:35 +0900

    1st commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { studioTheme } from '@/ui-components'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import { Amplify } from 'aws-amplify'
 import type { AppProps } from 'next/app'
+import config from '../aws-exports'
+
+Amplify.configure(config)
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <AmplifyProvider theme={studioTheme}><Component {...pageProps} /></AmplifyProvider>
 }
